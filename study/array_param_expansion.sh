2573850ccbe5a07ce9bb8d4c67e24eef608abd5f
13byte <13bitadc@naver.com>
2024-12-03 12:05:41 +0900

    Add examples of [@] and [*] array parameter expansion

---
+++ study/array_param_expansion.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+echo "@는 각 요소를 하나의 완전한 문자열로 처리"
+echo "*는 공백을 기준으로 분리되어 처리\n"
+FILE_EXTENSIONS=("hello world.ini" "test file.txt" ".c")
+
+echo "[*] 결과: $FILE_EXTENSIONS[*]"
+echo "-------------------"
+echo "[@] 결과: $FILE_EXTENSIONS[@]\n"
+
+echo "[@] 결과: \"${FILE_EXTENSIONS[@]}\""
+echo "-----------------------------------"
+echo "[*] 결과: \"${FILE_EXTENSIONS[*]}\"\n"
+
+IFS=","
+
+echo "IFS 적용 후 결과\n"
+echo "[*] 결과: $FILE_EXTENSIONS[*]"
+echo "-------------------"
+echo "[@] 결과: $FILE_EXTENSIONS[@]\n"
+
+IFS=" "
+
+echo "[@] 순회:"
+for file in "${FILE_EXTENSIONS[@]}"; do
+    echo "항목: $file"
+done
+
+echo "-----------------------------------"
+
+# [*] 사용
+echo "[*] 순회:"
+for file in "${FILE_EXTENSIONS[*]}"; do
+    echo "항목: $file"
+done
